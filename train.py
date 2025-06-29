b96c7e23a38c7629706fe110a33e230a37e21ec4
Tomás Marques <tomas.luis.marques@gmail.com>
2025-06-30 00:56:55 +0100

    Enhance MetricsLogger initialization to unpack model parameters for improved validation handling

--- train.py
+++ train.py
@@ -229,7 +229,8 @@ def main(model_params):
 
     if val_gen is not None:
         validation_steps = None if num_val_files == 0 else num_val_files//model_params['batch_size'],
-        metrics_logger = MetricsLogger(validation_steps, pose_annotations_file=model_params['val_annotations'], metrics_save_dir=metrics_save_dir, in_memory_generator=model_params['in_memory_generator_val'], model_params)
+        metrics_logger = MetricsLogger(validation_steps, pose_annotations_file=model_params['val_annotations'], 
+                                       metrics_save_dir=metrics_save_dir, in_memory_generator=model_params['in_memory_generator_val'], **model_params)
         callbacks.append(metrics_logger)
 
     # Print the labels for the first 2 batches
