387d7f9bdd820f351e2c84c5bd370c488722b456
Tomás Marques <tomas.luis.marques@gmail.com>
2025-08-02 18:52:44 +0100

    Increase the number of training epochs from 50 to 300 for improved model training

--- train.py
+++ train.py
@@ -1788,7 +1788,7 @@ if __name__ == "__main__":
         "joints_num": 25, # 24 for MP
         "num_classes": 14, # Number of classes for classification (NTU-120 has 120, MP has 12 and Therapies has 14)
 
-        "epochs": 50, # Number of training epochs
+        "epochs": 300, # Number of training epochs
         "n_trials": 20,  # Number of trials for Optuna
 
         # Set to 0 for no training logs, 1 for basic logs, >1 for more detailed logs
